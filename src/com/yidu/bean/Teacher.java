f58f5fb0181f1df9511ea9462602f5618e9a57c0
LvMuYing <3430163455@qq.com>
2020-12-24 15:14:01 +0800

    这是我的第二次提交

---
+++ src/com/yidu/bean/Teacher.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @description:
+ * @author: LvFang
+ * date:2020/12/24 15:12
+ * version:1.0
+ */
+public class Teacher {
+    public static void main(String[] args) {
+        System.out.println("Teacher");
+    }
+}
