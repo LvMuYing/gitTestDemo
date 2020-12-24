c3700852b299d407c556335db50bbd0fecc96432
LvMuYing <3430163455@qq.com>
2020-12-24 15:08:26 +0800

    这是我的第一次提交

---
+++ src/com/yidu/bean/Student.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @description:
+ * @author: LvFang
+ * date:2020/12/24 14:52
+ * version:1.0
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
